1035b656a61d043981ce277d04a90c9930847eb2
Frank Betancur <alejok6@gmail.com>
2022-10-08 21:36:59 -0500

    feature: add sass and node-sass
    feature: add under work page

---
+++ xkweb-web-app/src/components/atWork/AtWork.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.scss'
+
+const AtWork = () => {
+  return (
+    <div className='container'>
+      <img
+        src="https://res.cloudinary.com/panda2me/image/upload/v1665280522/xkweb-site/xkwebLogo_bh9jni.jpg"
+        alt="Logo"
+      />
+      <p>Under construction...</p>
+    </div>
+  )
+}
+
+export default AtWork
