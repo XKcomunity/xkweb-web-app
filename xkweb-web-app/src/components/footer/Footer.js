5e22f5419369e34071cfae6db1d6f2f060d1eedd
Frank Alejo Betancur <alejok6@gmail.com>
2022-10-27 21:22:11 -0500

    feature: add footer example (#5)

---
+++ xkweb-web-app/src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.scss'
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <img
+        src="https://res.cloudinary.com/panda2me/image/upload/v1665280522/xkweb-site/xkwebLogo_bh9jni.jpg"
+        alt="Logo"
+      />
+      <p>XK Web</p>
+    </div>
+  );
+};
+
+export default Footer;
