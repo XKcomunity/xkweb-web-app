5a6ca2baf82094e0429c221eab3a23cbea13de82
Andrés de Jesús Quesada Cedeño <92536363+AJMS1118@users.noreply.github.com>
2023-02-03 08:10:24 -0500

    Home (#7)
    Co-authored-by: AJMS1118 <ajquesadac90@gmail.com>

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,6 @@ import { NavBar } from "@/components/complex/navbar/NavBar";
 import { SliderCard } from "@/components/complex/slider/SliderCard";
 
 export default function Home() {
-	const image1 =
-		"https://images.unsplash.com/photo-1499673610122-01c7122c5dcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGVuZ3VhamVzJTIwZGUlMjBwcm9ncmFtYWNpJUMzJUIzbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60";
-
-export default function Home() {
 	// const image1 =
 	// 	"https://images.unsplash.com/photo-1499673610122-01c7122c5dcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGVuZ3VhamVzJTIwZGUlMjBwcm9ncmFtYWNpJUMzJUIzbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60";
 
