1361324bce7ad6b98ca062059f2d6b95ceca425f
AJMS1118 <ajquesadac90@gmail.com>
2023-02-04 00:32:43 -0500

    animaciones

--- src/components/complex/HomeBanner/HomeBanner.jsx
+++ src/components/complex/HomeBanner/HomeBanner.jsx
@@ -1,29 +1,39 @@
+"use client";
 import Image from "next/image";
 import styles from "./home-banner.module.scss";
-// import bg from "../../../../public/images/bg.png";
 import robot from "../../../../public/images/robots/robot.png";
-// import space from "../../../../public/images/space.jpg";
+import { motion } from "framer-motion";
 export const HomeBanner = () => {
 	return (
 		<>
 			<div className={styles.image_background}>
 				<div className={styles.space_background}></div>
-				<div>
-					<Image
-						className={styles.responsive_image}
-						src={robot}
-						width={250}
-						height={250}
-						style={{
-							position: "absolute",
-							objectFit: "cover",
+				<motion.div
+					whileInView={{ translateY: [-20, 20] }}
+					transition={{
+						delayChildren: 0.5,
+						duration: 2,
+						repeat: Infinity,
+						repeatType: "reverse",
+					}}
+				>
+					<div>
+						<Image
+							className={styles.responsive_image}
+							src={robot}
+							width={250}
+							height={250}
+							style={{
+								position: "absolute",
+								objectFit: "cover",
 
-							top: 180,
-							left: 350,
-						}}
-						alt="robot image"
-					/>
-				</div>
+								top: 180,
+								left: 350,
+							}}
+							alt="robot image"
+						/>
+					</div>
+				</motion.div>
 			</div>
 		</>
 	);
--- src/components/complex/navbar/NavBar.jsx
+++ src/components/complex/navbar/NavBar.jsx
@@ -1,4 +1,7 @@
+"use client";
+import Link from "next/link";
 import styles from "./navbar.module.scss";
+import { motion } from "framer-motion";
 export const NavBar = () => {
 	return (
 		<>
@@ -6,12 +9,54 @@ export const NavBar = () => {
 				<div className={styles.navbar_logo}>Xk Web</div>
 				<div className={styles.navbar_pages}>
 					<ul className={styles.navbar_list}>
-						<li className={styles.navbar_item}>Home</li>
-						<li className={styles.navbar_item}>Blog</li>
-						<li className={styles.navbar_item}>Cursos</li>
-						<li className={styles.navbar_item}>About us</li>
-						<li className={styles.navbar_item}>Login</li>
-						<li className={styles.navbar_item}>Register</li>
+						<motion.li
+							whileHover={{ translateY: -5 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="#">
+								<li className={styles.navbar_item}>Home</li>
+							</Link>
+						</motion.li>
+						<motion.li
+							whileHover={{ translateY: -5 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="#">
+								<li className={styles.navbar_item}>Blog</li>
+							</Link>
+						</motion.li>
+						<motion.li
+							whileHover={{ translateY: -5 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="#">
+								<li className={styles.navbar_item}>Cursos</li>
+							</Link>
+						</motion.li>
+						<motion.li
+							whileHover={{ translateY: -5 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="#">
+								<li className={styles.navbar_item}>About us</li>
+							</Link>
+						</motion.li>
+						<motion.li
+							whileHover={{ translateY: -5 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="#">
+								<li className={styles.navbar_item}>Login</li>
+							</Link>
+						</motion.li>
+						<motion.li
+							whileHover={{ translateY: -5 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Link href="#">
+								<li className={styles.navbar_item}>Register</li>
+							</Link>
+						</motion.li>
 					</ul>
 				</div>
 			</nav>
