3d6dba31da5ecf24907fcb469de5ea0c3466b6f2
AJMS1118 <ajquesadac90@gmail.com>
2023-02-08 13:10:05 -0500

    section 7

--- src/components/complex/navbar/NavBar.jsx
+++ src/components/complex/navbar/NavBar.jsx
@@ -3,14 +3,36 @@ import Link from "next/link";
 import styles from "./navbar.module.scss";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { HiMenu } from "react-icons/hi";
 
 import logo from "../../../../public/logo/logo_3.png";
+// import mobile_logo from "../../../../public/logo/logo_mobile.svg";
 export const NavBar = () => {
 	return (
 		<>
 			<nav className={styles.navbar_wrapper}>
 				<div className={styles.navbar_logo}>
-					<Image src={logo} alt="" width={170} height={110} />
+					<Image
+						className={styles.logo_image}
+						src={logo}
+						alt=""
+						width={170}
+						height={110}
+					/>
+					{/* <Image
+						className={styles.mobile_logo_image}
+						src={mobile_logo}
+						width={60}
+						height={60}
+						alt="mobile logo"
+						onClick={() => alert("Abriendo el Drawer")}
+					/> */}
+					<HiMenu
+						size={60}
+						color="white"
+						className={styles.mobile_logo_image}
+						onClick={() => alert("Abriendo el Drawer")}
+					/>
 				</div>
 				<div className={styles.navbar_pages}>
 					<ul className={styles.navbar_list}>
