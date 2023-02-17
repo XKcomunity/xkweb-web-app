73f2f22f4c4e082f74af98af204821f50641742f
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-02-17 23:17:17 +0100

    feature: Agregue ruta de snippet y configure el navbar y el footer como componentes  globales.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,11 @@ import { MainTitle } from "@/components/basic/main_title/MainTitle";
 import { SecondaryTitle } from "@/components/basic/secondary_title/SecondaryTitle";
 import { HomeBanner } from "@/components/complex/HomeBanner/HomeBanner";
 import { HomeSection } from "@/components/complex/home-section/HomeSection";
-import { NavBar } from "@/components/complex/navbar/NavBar";
 import { SliderCard } from "@/components/complex/slider/SliderCard";
-import { Footer } from "@/components/complex/footer/Footer";
 
 export default function Home() {
 	return (
 		<>
-			<NavBar />
 			<HomeBanner />
 			<MainTitle />
 			<HomeSection />
@@ -18,7 +15,6 @@ export default function Home() {
 			<SliderCard />
 			<SecondaryTitle secondary_title="Backend" />
 			<SliderCard />
-			<Footer />
 		</>
 	);
 }
---
+++ src/app/snippets/MainTitle_snippet.jsx
@@ -0,0 +1,13 @@
+"use client";
+import styles from "../snippets/_snippetPage.module.scss";
+
+export default function MainTitle_snipet() {
+	return (
+		<>
+		   <main className={styles.main_snippet}>
+				<h1 className={styles.h1}>Explora Por <span className={styles.h1_span}>Temas</span></h1>
+				<p className={styles.headings_snippets_text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt provident eius ratione similique autem debitis laboriosam obcaecati a eveniet modi quisquam harum dolorem eligendi commodi ipsam repellat eos, sequi sapiente.</p>
+		   </main>
+		</>
+	);
+}
