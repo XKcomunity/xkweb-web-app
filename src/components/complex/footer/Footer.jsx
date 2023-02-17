521f52ae1bb3b8d73b0e8f40b10977faba4764e4
XKweb <112565583+XKcomunity@users.noreply.github.com>
2023-02-17 18:54:58 +0100

    feature: Cree el footer de y removi propiedades in-necesarias en los … (#24)
    …estilos.
    
    Co-authored-by: Michael Linares <michaellinaresdbildungs@gmail.com>

---
+++ src/components/complex/footer/Footer.jsx
@@ -0,0 +1,70 @@
+"use client";
+import styles from "./footer.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { FaInstagram } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
+import logo from "../../../../public/logo/logo-x.png";
+
+export function Footer() {
+	return (
+		<>
+			<footer className={styles.footer}>
+				<div className={styles.footer_wrapper}>
+					<div className={styles.footer_section_1}>
+						<div className={styles.footer_logo}>
+							<Image src={logo} width={90} height={90} alt="" />
+							<span className={styles.logo_text}>XK Community</span>
+						</div>
+						<div className={styles.footer_description}>
+							As the ultimate resource for Vue.js developers, Vue Mastery produces
+							weekly lessons so you can learn what you need to succeed as a Vue.js
+							Developer
+						</div>
+						<div className={styles.footer_social_media}>
+							<div className={styles.social_icons_wrapper}>
+								<motion.div whileHover={{ scale: 1.1 }}>
+									<div>
+										<FaInstagram color="white" size={35} />
+									</div>
+								</motion.div>
+								<motion.div whileHover={{ scale: 1.1 }}>
+									<div>
+										<FaDiscord color="white" size={35} />
+									</div>
+								</motion.div>
+								<motion.div whileHover={{ scale: 1.1 }}>
+									<div>
+										<FaYoutube color="white" size={35} />
+									</div>
+								</motion.div>
+							</div>
+						</div>
+					</div>
+					<div className={styles.footer_section_2}>
+						<div className={styles.links_heading}>
+							<span className={styles.heading}>Enlaces</span>
+						</div>
+						<div className={styles.footer_links}>
+							<Link href="#" className={styles.link}>
+								Inicio
+							</Link>
+							<Link href="#" className={styles.link}>
+								Artículos
+							</Link>
+							<Link href="#" className={styles.link}>
+								Snippets
+							</Link>
+							<Link href="#" className={styles.link}>
+								Contacto
+							</Link>
+						</div>
+					</div>
+				</div>	
+			</footer>
+		</>
+	);
+}
