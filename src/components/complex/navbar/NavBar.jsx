1de8825a01fb0ec68cde6bc0a67b50523b363b85
AJMS1118 <ajquesadac90@gmail.com>
2023-02-18 22:32:09 -0500

    feature: se añadió el drawer usando el paquete react-modern-drawer

--- src/components/complex/navbar/NavBar.jsx
+++ src/components/complex/navbar/NavBar.jsx
@@ -1,15 +1,28 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./navbar.module.scss";
 import { motion } from "framer-motion";
 import { HiMenu } from "react-icons/hi";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
+import { useState } from "react";
+
+import logo from "../../../../public/logo/logo-vertical.png";
 
 export const NavBar = () => {
+	const width = window.visualViewport.width / 2;
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const toogleDrawer = () => {
+		setMenuOpen((state) => !state);
+	};
+
 	return (
 		<>
 			<nav className={styles.navbar_wrapper}>
 				<div className={styles.navbar_logo}>
-				    <h2>XK-Comunity</h2>
+					<h2>XK-Comunity</h2>
 				</div>
 				<div className={styles.navbar_pages}>
 					<ul className={styles.navbar_list}>
@@ -19,7 +32,7 @@ export const NavBar = () => {
 						>
 							<Link href="/">
 								<li className={styles.navbar_item}>
-								   <a>Inicio</a>
+									<a>Inicio</a>
 								</li>
 							</Link>
 						</motion.li>
@@ -50,12 +63,42 @@ export const NavBar = () => {
 					</ul>
 				</div>
 				<HiMenu
-						size={45}
-						color="white"
-						className={styles.mobile_logo_image}
-						onClick={() => alert("Abriendo el Drawer")}
-					/>
+					size={45}
+					color="white"
+					className={styles.mobile_logo_image}
+					onClick={toogleDrawer}
+				/>
 			</nav>
+
+			<Drawer
+				open={menuOpen}
+				onClose={toogleDrawer}
+				direction="right"
+				size={width}
+				duration={400}
+				style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+			>
+				<div className={styles.drawer_logo_wrapper}>
+					<Image src={logo} width={135} height={135} alt="mobile menu logo" />
+				</div>
+				<hr style={{ lineHeight: "5", color: "white" }} />
+				<div className={styles.drawer_links}>
+					<ul className={styles.list_links}>
+						<li className={styles.link}>
+							<Link href="#">Inicio</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href="#">Artículos</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href="#">Snippets</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href="#">Contacto</Link>
+						</li>
+					</ul>
+				</div>
+			</Drawer>
 		</>
 	);
 };
