39edb9a35a0f7be86fc18d9eedb8c05012aeaf8b
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2023-02-21 10:41:48 +0100

    Feature: cambie los snippet heading links de <a> a <button> y removi … (#32)
    …algunos estilos innecesarios.

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -5,7 +5,7 @@ export default function Head() {
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta
 				name="description"
-				content="Un sitio web para aprender sobre el maravilloso mundo de la programación"
+				content="plataforma para compartir snippet de código, somos la oportunidad para mejorar tus habilidades y ser parte de una comunidad apasionada que comparte tu interés  por la programación."
 			/>
 			<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 		</>
