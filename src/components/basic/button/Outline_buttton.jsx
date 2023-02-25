22b0947c030a3bf965b5e51d799dcc9a91e1291a
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2023-02-25 19:44:32 +0100

    feature: Agregue botones en el home para llevar a la ruta articulos y… (#40)
    <img width="1459" alt="Screenshot 2023-02-25 at 19 42 41"
    src="https://user-images.githubusercontent.com/116801315/221374221-c0359790-50b7-4aec-ac39-30ac27da9fd8.png">
    … snippets.

--- src/components/basic/button/Outline_buttton.jsx
+++ src/components/basic/button/Outline_buttton.jsx
@@ -1,17 +1,17 @@
 "use client";
 import styles from "./button.module.scss";
 
-const Button = () => {
+const OutlineButton = ({buttonText}) => {
 	const ShowMessage = () => {
 		alert("Welcome to Xk Web School");
 	};
 	return (
 		<>
-			<button className={styles.button} onClick={ShowMessage}>
-			   	Leer más
+			<button className={styles.outline_button} onClick={ShowMessage}>
+			   {buttonText}
 			</button>
 		</>
 	);
 };
 
-export default Button;
+export default OutlineButton;
