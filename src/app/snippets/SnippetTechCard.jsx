e09e93b1c418cb9551a215700c96158bc0627cf6
AJMS1118 <ajquesadac90@gmail.com>
2023-02-26 18:19:12 -0500

    feature: se cargó la informacion del db.json con el fetch

--- src/app/snippets/SnippetTechCard.jsx
+++ src/app/snippets/SnippetTechCard.jsx
@@ -1,17 +1,40 @@
-"use client";
+// "use client";
+import Image from "next/image";
 import styles from "../snippets/_snippetPage.module.scss";
 
-export default function SnippetTechCard() {
+async function fetchTechnologies() {
+	const tecnologiesResponse = await fetch(
+		"http://localhost:3001/technologies",
+		{
+			cache: "no-store",
+		}
+	);
+
+	return tecnologiesResponse.json();
+}
+
+export default async function SnippetTechCard() {
+	const technology = await fetchTechnologies();
 	return (
 		<>
-            <div className={styles.techcard_container}>
-                <div className={styles.tech_card}>
-                    <div className={styles.tech_card_img_content}></div>
-                    <div className={styles.tech_card_title_content}>
-                        <h4 className={styles.tech_card_title}>javascript</h4>
-                    </div>
-                </div>
-            </div>
+			{technology.map((tecno) => (
+				<div key={tecno.id} className={styles.techcard_container}>
+					<div className={styles.tech_card}>
+						<div className={styles.tech_card_img_content}>
+							<Image
+								src={tecno.img}
+								alt=""
+								// style={{ width: "100%", height: "100%", objectFit: "cover" }}
+								width={50}
+								height={50}
+							/>
+						</div>
+						<div className={styles.tech_card_title_content}>
+							<h4 className={styles.tech_card_title}>{tecno.title}</h4>
+						</div>
+					</div>
+				</div>
+			))}
 		</>
 	);
 }
--- src/app/snippets/page.jsx
+++ src/app/snippets/page.jsx
@@ -1,19 +1,19 @@
-"use client";
 import styles from "../snippets/_snippetPage.module.scss";
 import MainTitle_snipet from "./MainTitle_snippet";
 import SnippetHeadingLinks from "./SnippetHeadingLinks";
 import SnippetTechCard from "./snippetTechCard";
 
-
 export default function Snippet() {
 	return (
 		<>
-		   <main className={styles.main_snippet}>
+			<main className={styles.main_snippet}>
 				<MainTitle_snipet />
 				<SnippetHeadingLinks />
 				<div className={styles.divider}></div>
-				<SnippetTechCard />
-		   </main>
+				<div className={styles.tech_cards_wrapper}>
+					<SnippetTechCard />
+				</div>
+			</main>
 		</>
 	);
 }
