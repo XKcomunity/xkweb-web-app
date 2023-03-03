e101c5896478083e22aed344b9738c331870281e
AJMS1118 <ajquesadac90@gmail.com>
2023-03-03 00:28:54 -0500

    feature: creado el fetch de las categorias

--- src/app/snippets/SnippetHeadingLinks.jsx
+++ src/app/snippets/SnippetHeadingLinks.jsx
@@ -1,22 +1,39 @@
 import styles from "../snippets/_snippetPage.module.scss";
 
-async function fetchCategories() {
-	const categoriesResponse = await fetch(" http://localhost:3001/categories", {
-		cache: "no-store",
-	});
+async function fetchTechnologies() {
+	const tecnologiesResponse = await fetch(
+		"http://localhost:3001/technologies",
+		{
+			cache: "no-store",
+		}
+	);
 
-	return categoriesResponse.json();
+	return tecnologiesResponse.json();
 }
 
 export default async function SnippetHeadingLinks() {
-	const categories = await fetchCategories();
+	const tecnology = await fetchTechnologies();
+
+	const categories = tecnology.filter(
+		(tecno, index) =>
+			tecnology.findIndex((item) => item.category === tecno.category) === index
+	);
+
+	// console.log(categories);
+
 	return (
 		<>
 			<div className={styles.headings_links_container}>
-				<button className={styles.snippetHeadingLinks}>{categories[3]}</button>
+				<button className={styles.snippetHeadingLinks}>Todos</button>
+				{categories.map((tecnoItem) => (
+					<button className={styles.snippetHeadingLinks} key={tecnoItem.id}>
+						{tecnoItem.category}
+					</button>
+				))}
+				{/* <button className={styles.snippetHeadingLinks}>{categories[3]}</button>
 				<button className={styles.snippetHeadingLinks}>{categories[2]}</button>
 				<button className={styles.snippetHeadingLinks}>{categories[1]}</button>
-				<button className={styles.snippetHeadingLinks}>{categories[0]}</button>
+				<button className={styles.snippetHeadingLinks}>{categories[0]}</button> */}
 			</div>
 		</>
 	);
