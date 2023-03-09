d92e73dcd5bf921f6025b30767f3145091fba2e8
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-03-09 18:53:24 +0100

    feature: Realizé fetch para los snippetTechPils ahora son dinamicos y unicos.

--- src/app/snippet-tech/SnippetTechPils.jsx
+++ src/app/snippet-tech/SnippetTechPils.jsx
@@ -1,29 +1,37 @@
-// "use client";
+// "use client"
 import styles from "./snippet-tech.module.scss";
 
-export default function SnippetTechPils() {
-	return (
-		<>
-			<section className={styles.snippet_tech_wrapper}>
+async function fetchSnippetPils() {
+	const pilsListResponse = await fetch("http://localhost:3001/snippetCardList", {
+		cache: "no-store",
+	});
+    const pilsList = pilsListResponse.json();
+    return pilsList;
+}
+
+export default async function SnippetTechPils() {
+
+    const snippetPilsList = await fetchSnippetPils();
+    const uniquePils = snippetPilsList.filter((pils, index, arr) => {
+        return arr.findIndex((p) => p.topic === pils.topic) === index;
+      });
+
+    // const handlePilsButtonClick = (topic) => {
+    //     return snippetPilsList.filter((pils) => pils.topic === topic)
+    // };
+
+    //  onClick={() => handlePilsButtonClick(pils.topic)}
+
+    return (
+        <>
+            <section className={styles.snippet_tech_wrapper}>
                 <article className={styles.snippet_tech_content}>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
-                    <button className={styles.pils_button}>array</button>
+                    {uniquePils.map((pils) => (
+                       <button className={styles.pils_button} key={pils.id}
+                           >{pils.topic}</button>
+                    ))}
                 </article>
             </section>
-		</>
-	);
+        </>
+    );
 }
