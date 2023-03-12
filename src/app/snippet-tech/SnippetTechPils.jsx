e702311269d2da9c7646026142864578f3832ea1
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-03-12 02:38:11 +0100

    feature: Actualize el codigo para realizar fetch a react hooks y logre filtrar correctamente los botones.

--- src/app/snippet-tech/SnippetTechPils.jsx
+++ src/app/snippet-tech/SnippetTechPils.jsx
@@ -1,37 +1,21 @@
-// "use client"
+
 import styles from "./snippet-tech.module.scss";
 
-async function fetchSnippetPils() {
-	const pilsListResponse = await fetch("http://localhost:3001/snippetCardList", {
-		cache: "no-store",
-	});
-    const pilsList = pilsListResponse.json();
-    return pilsList;
-}
+export default function SnippetTechPils({ snippetCardList, onTopicClick }) {
+  const uniquePils = [...new Set(snippetCardList.map((item) => item.topic))];
 
-export default async function SnippetTechPils() {
-
-    const snippetPilsList = await fetchSnippetPils();
-    const uniquePils = snippetPilsList.filter((pils, index, arr) => {
-        return arr.findIndex((p) => p.topic === pils.topic) === index;
-      });
-
-    // const handlePilsButtonClick = (topic) => {
-    //     return snippetPilsList.filter((pils) => pils.topic === topic)
-    // };
-
-    //  onClick={() => handlePilsButtonClick(pils.topic)}
-
-    return (
-        <>
-            <section className={styles.snippet_tech_wrapper}>
-                <article className={styles.snippet_tech_content}>
-                    {uniquePils.map((pils) => (
-                       <button className={styles.pils_button} key={pils.id}
-                           >{pils.topic}</button>
-                    ))}
-                </article>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className={styles.snippet_tech_wrapper}>
+        <article className={styles.snippet_tech_content}>
+          {uniquePils.map((pils, index) => (
+            <button className={styles.pils_button} key={index}
+            onClick={() => onTopicClick(pils)}>
+              {pils}
+            </button>
+          ))}
+        </article>
+      </section>
+    </>
+  );
 }
--- src/app/snippets/SnippetHeadingLinks.jsx
+++ src/app/snippets/SnippetHeadingLinks.jsx
@@ -1,28 +1,19 @@
-
 import styles from "../snippets/_snippetPage.module.scss";
 
-async function fetchTechnologies() {
-    const url = "http://localhost:3001/categoryLinks";
-    const response = await fetch(url, { cache: "no-store" });
-    const categoryLinkList = response.json();
-    return categoryLinkList;
-  }
+export default function SnippetHeadingLinks({ technologies, handleCategoryClick }) {
 
-  export default async function SnippetHeadingLinks() {
-
-    const categoryLinks = await fetchTechnologies();
-    const uniqueCategories = Array.from(new Set(categoryLinks.map((category) => category)));
+    const uniqueCategories = [...new Set(technologies.map((item) => item.category))];
 
     return (
         <>
             <div className={styles.headings_links_container}>
-                {uniqueCategories.map((category) => (
-                <button className={styles.snippetHeadingLinks} key={category}>
-                    {category}
+                {uniqueCategories.map((cat,index) => (
+                <button className={styles.snippetHeadingLinks} key={index}
+                onClick={() => handleCategoryClick(cat)}>
+                    {cat}
                 </button>
                 ))}
-
             </div>
         </>
     );
-  }
+}
