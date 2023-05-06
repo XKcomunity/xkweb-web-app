7591f213c374d93d5419eee2e8f145af66ab52e2
AJMS1118 <ajquesadac90@gmail.com>
2023-05-06 13:21:48 -0400

    trying again the pr

--- src/app/docs/WrapperButtons.tsx
+++ src/app/docs/WrapperButtons.tsx
@@ -6,10 +6,10 @@ import styles from "./docs.module.scss";
 export default function Docs() {
 	return (
 		<>
-            <div className={styles.button_wrapper}>
-                <Button buttonText="poner texto" />
-                <OutlineButton buttonText="mas info" />
-            </div>
+			<div className={styles.button_wrapper}>
+				<Button buttonText="poner texto" />
+				<OutlineButton buttonText="mas info" />
+			</div>
 		</>
 	);
 }
