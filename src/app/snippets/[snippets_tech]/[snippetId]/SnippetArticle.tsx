37d23bd27f76f0dc8060a27be56d4cc743309be0
Michael Linares <michaellinaresdbildungs@gmail.com>
2023-05-07 20:31:17 +0200

    Creé funcionalidad para que el highlight de el código sea eficiente, ahora tenemos substring para las snippetCardList ahora solo mostrarán 200 carácteres.

--- src/app/snippets/[snippets_tech]/[snippetId]/SnippetArticle.tsx
+++ src/app/snippets/[snippets_tech]/[snippetId]/SnippetArticle.tsx
@@ -6,25 +6,70 @@ import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 type LibraryCode = {
     title: string;
     code: string;
-  };
+};
 
-  type Props = {
+type Props = {
     description: string;
-    code: LibraryCode;
-  };
+};
 
   export default function SnippetArticle({ description }: Props) {
-    const [text, code] = description.split('```');
+    const codeBlocks: LibraryCode[] = [];
+    let text = description;
+  
+    // Encuentra todos los bloques de código
+    const codeRegex = /```([\w\W]+?)```/g;
+    let match = codeRegex.exec(description);
+    while (match) {
+      const code = match[1];
+      const titleMatch = /title:(.*)/.exec(code);
+      const title = titleMatch ? titleMatch[1].trim() : 'Code';
+      codeBlocks.push({ title, code });
+      match = codeRegex.exec(description);
+    }
 
+    // Separa el texto en bloques de texto y los mezcla con los bloques de código
+    const blocks: (LibraryCode | string)[] = [];
+    const textRegex = /```([\w\W]+?)```/;
+    let blockMatch = textRegex.exec(text);
+    while (blockMatch) {
+      if (blockMatch.index > 0) {
+        blocks.push(text.slice(0, blockMatch.index));
+      }
+
+      const codeBlockIndex = codeBlocks.findIndex(
+        (block) => block.code === blockMatch[1]
+      );
+      if (codeBlockIndex >= 0) {
+        blocks.push(codeBlocks[codeBlockIndex]);
+        codeBlocks.splice(codeBlockIndex, 1);
+      }
+
+      text = text.slice(blockMatch.index + blockMatch[0].length);
+      blockMatch = textRegex.exec(text);
+    }
+
+    if (text) {
+      blocks.push(text);
+    }
+    blocks.push(...codeBlocks);
+
+    // Renderiza los bloques de texto y los bloques de código en el orden correcto
     return (
       <section className={styles.snippet_article_content}>
-        <p className={styles.article_text}>{text}</p>
-        <div>
-          <SyntaxHighlighter language="typescript" style={atomDark}>
-            {code}
-          </SyntaxHighlighter>
-        </div>
+        {blocks.map((block, index) =>
+          typeof block === 'string' ? (
+            <p key={index} className={styles.article_text}>
+              {block}
+            </p>
+          ) : (
+            <div key={index}>
+              <h3>{block.title}</h3>
+              <SyntaxHighlighter language="typescript" style={atomDark}>
+                {block.code}
+              </SyntaxHighlighter>
+            </div>
+          )
+        )}
       </section>
     );
   }
-
