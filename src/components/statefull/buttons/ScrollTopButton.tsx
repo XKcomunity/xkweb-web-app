1247a26943668c236d2bc07a58803e2e84cf6f2d
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2023-05-21 19:17:37 +0200

    Refactor: refactorizé todo el codigo existente de css creando variabl… (#89)
    …es y eliminando propiedades duplicadas en los estilos.

--- src/components/statefull/buttons/ScrollTopButton.tsx
+++ src/components/statefull/buttons/ScrollTopButton.tsx
@@ -1,15 +1,15 @@
-'use client';
-import { useScrollTop } from '@/hooks/useScrollTop';
-import { scrollToTop } from '@/utils/scrollToTop';
-import styles from './scroll-button.module.scss';
-import { BsArrowUpShort } from 'react-icons/bs';
+"use client";
+import { useScrollTop } from "@/hooks/useScrollTop";
+import { scrollToTop } from "@/utils/scrollToTop";
+import styles from "../../stateless/buttons/button.module.scss";
+import { BsArrowUpShort } from "react-icons/bs";
 
 const ScrollTopButton = () => {
 	const { isVisible } = useScrollTop();
 	return (
 		<button
 			className={styles.scroll_btn}
-			style={{ display: isVisible ? 'block' : 'none' }}
+			style={{ display: isVisible ? "block" : "none" }}
 			onClick={scrollToTop}
 			title="icon"
 		>
