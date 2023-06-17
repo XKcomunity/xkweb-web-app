a33545723a85429a042ee90e29c15da0ed89e417
XKweb <112565583+XKcomunity@users.noreply.github.com>
2023-06-17 21:00:31 +0200

    Creé types para author y configure la data del componente en otras vi… (#118)
    …stas.
    
    Co-authored-by: Michael Linares <michaellinaresdbildungs@gmail.com>

--- src/components/stateless/author-info/SotialNetwork.tsx
+++ src/components/stateless/author-info/SotialNetwork.tsx
@@ -1,21 +1,28 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 import styles from './author-info.module.scss';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 type AuthorSotialNetwork = {
+  sotial: SotialNetworks;
 }
 
-export function AuthorSotialNetwork(){
+export function AuthorSotialNetwork({sotial}: AuthorSotialNetwork){
   return(
     <section className={styles.author_sotialnetworks}>
-      <Link href="https://www.google.com/" target='_blank'>
-        Linkedin
-      </Link>
-      <Link href="https://www.google.com/" target='_blank'>
-        twitter
-      </Link>
-      <Link href="https://www.google.com/" target='_blank'>
-        github
-      </Link>
+      	<FaLinkedin size={24}/>
+        <FaTwitter size={24}/>
+        <FaGithub size={24}/>
+      {/* <Link href={data.author.linkedIn} target='_blank'>
+				<FaLinkedin size={24}/>
+			</Link>
+      <Link href={data.author.twitter} target='_blank'>
+				<FaTwitter size={24}/>
+			</Link>
+      <Link href={data.author.github} target='_blank'>
+				<FaGithub size={24}/>
+			</Link> */}
     </section>
   );
 }
