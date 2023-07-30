e1de45a887c265e764b38112d71f6489c0b49b5a
Michael Linares <116801315+Michaellinaresxk@users.noreply.github.com>
2023-07-30 16:21:40 +0200

    Agregué estilos como también textos y un banner para la vista /docs. (#139)
    ![screencapture-localhost-3000-docs-2023-07-30-16_19_14](https://github.com/XKcomunity/xkweb-web-app/assets/116801315/1990b07a-42b4-423e-a982-5078d9a70c18)

--- src/components/stateless/pdf/PdfCard.tsx
+++ src/components/stateless/pdf/PdfCard.tsx
@@ -1,9 +1,17 @@
+import styles from "./pdf.module.scss";
+import pdf from "../../../../public/images/pdf/vue 1.jpg";
+import Image from "next/image";
+
 
 export default function PdfCard() {
 
   return (
     <>
-      <h1>ahora estoy</h1>
+      <div className={styles.pdfContent}>
+         <Image className={styles.pdf_label} src={pdf} alt="" width={250} height={310} />
+         <Image className={styles.pdf_label} src={pdf} alt="" width={250} height={310}/>
+         <Image className={styles.pdf_label} src={pdf} alt="" width={250} height={310}/>
+      </div>
     </>
   )
 
